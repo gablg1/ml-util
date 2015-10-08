d0e758f7e6801cad31c1662ee161c781fddc0289
Brian Arroyo <brianarroyo@college.harvard.edu>
2015-10-08 17:46:16 -0400

    added kwargs to some plot functions

--- simple_plot.py
+++ simple_plot.py
@@ -6,14 +6,14 @@ class Plotter():
     def __init__(self, x_min=-1, x_max=1, nsamples=100):
         self.x_plot = np.linspace(x_min, x_max, nsamples)
 
-    def plotData(self, x, y):
-        plt.scatter(x, y)
+    def plotData(self, x, y, *args, **kwargs):
+        plt.scatter(x, y, *args, **kwargs)
 
     def plotFunction(self, f):
         vf = np.vectorize(f)
         y_plot = vf(self.x_plot)
         assert(self.x_plot.shape == y_plot.shape)
-        plt.plot(self.x_plot, y_plot, '-')
+        plt.plot(self.x_plot, y_plot, self.args, self.kwargs)
 
     def show(self):
         plt.show()
